fe04157a936fa69bdf340f3d303a624e80708d44
HarshaSalih <harshasalih2000@gmail.com>
2022-11-22 06:43:02 +0530

    commits

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,32 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+class Student
+{
+    String name;
+    int admnNo;
+    int rollNo;
+    String college;
+
+    public Student(String name, int admnNo, int rollNo, String college) {
+        this.name = name;
+        this.admnNo = admnNo;
+        this.rollNo = rollNo;
+        this.college = college;
+    }
+}
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        ArrayList<Student> arrStu = new ArrayList<Student>();
+        while (true) {
+            System.out.println("Select an option\n1.Add student\n2.View student\n3.Search student\n4.Delete student\n5.Exit");
+            Scanner sc = new Scanner(System.in);
+            int option = sc.nextInt();
+            if (option == 5) {
+                System.out.println("You are exiting from the program");
+                System.exit(0);
+            } else {
+
+            }
+        }
     }
 }
